6868c84b610f9a415b24f3ea5c6a93347f2f777d
modern1s <soonhyung92@gmail.com>
2024-01-08 15:32:36 +0900

    [chore]inital setup

---
+++ src/main/kotlin/com/sparta/techTree/TechTreeApplication.kt
@@ -0,0 +1,11 @@
+package com.sparta.techTree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechTreeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TechTreeApplication>(*args)
+}
