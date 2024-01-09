42c2991cb3d9a119cca61ef64dcc48ae9afc8df3
rim109 <fla7722@naver.com>
2024-01-09 20:32:31 +0900

    feat: CommentDTO 수정

--- src/main/kotlin/com/sparta/techTree/comment/dto/CommentDTO.kt
+++ src/main/kotlin/com/sparta/techTree/comment/dto/CommentDTO.kt
@@ -3,7 +3,6 @@ package com.sparta.techTree.comment.dto
 import java.time.LocalDateTime
 
 data class CommentDTO (
-    val id: Long,
     val userId: Long,
     val postId: Long,
     val content: String,
