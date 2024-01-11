27ca53530ef2949fac8f37f86cf7f2451ee58ebc
Karox1234 <zzzcomet@gmail.com>
2024-01-11 17:14:48 +0900

    post like 예외처리 완료

--- src/main/kotlin/com/sparta/techTree/like/service/LikeServiceImpl.kt
+++ src/main/kotlin/com/sparta/techTree/like/service/LikeServiceImpl.kt
@@ -5,26 +5,32 @@ import com.sparta.techTree.like.dto.PostLikeResponse
 import com.sparta.techTree.like.model.Like
 import com.sparta.techTree.like.model.toResponse
 import com.sparta.techTree.like.repository.LikeRepository
+import com.sparta.techTree.post.model.Post
+import com.sparta.techTree.post.repository.PostRepository
 import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 
 @Service
-class LikeServiceImpl(private val likeRepository: LikeRepository) : LikeService {
+class LikeServiceImpl(private val likeRepository: LikeRepository,private val postRepository:PostRepository) : LikeService {
+
     @Transactional
     override fun toggleLikeForPost(postId: Long, userId: Long): PostLikeResponse {
-        //좋아요가 있나 확인
-        val existingLike = likeRepository.findByPostIdAndUserId(postId, userId)
-//            ?: throw ModelNotFoundException("Like", postId) 예외처리중 오류나서 해결중
-        //있을경우(if) 좋아요를 토글해서 저장
-        if (existingLike != null) {
-            existingLike.liked = !existingLike.liked
-            val updatedLike = likeRepository.save(existingLike)
-            return updatedLike.toResponse()
-            //없을경우 좋아요를 최초 생성하여 true(좋아요 상태)로 만듬
+        val post: Post = postRepository.findByIdOrNull(postId) ?: throw ModelNotFoundException("Post", postId)
+        if (post != null) {
+            val existingLike = likeRepository.findByPostIdAndUserId(postId, userId)
+            if (existingLike != null) {
+                existingLike.liked = !existingLike.liked
+                val updatedLike = likeRepository.save(existingLike)
+                return updatedLike.toResponse()
+            } else {
+                val newLike =
+                    likeRepository.save(Like(postId = postId, userId = userId, commentId = null, liked = true))
+                return newLike.toResponse()
+            }
         } else {
-            val newLike = likeRepository.save(Like(postId = postId, userId = userId, commentId = null, liked = true))
-            return newLike.toResponse()
+            throw ModelNotFoundException("Post", postId)
         }
     }
 }
